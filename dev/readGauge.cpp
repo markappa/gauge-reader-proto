0a6fe4902b4f1764513c2060f5165c5786fce4a1
Marco Mondini <markappa@users.noreply.github.com>
2021-12-13 19:13:23 +0100

    started adding options (not compiled)

--- dev/readGauge.cpp
+++ dev/readGauge.cpp
@@ -1,4 +1,20 @@
+/**
+TODO list:
+Accept calibration arguments from commandline
+Option to do Canny on input file
+commandline options to perform calibration:
+    step1 find center and radius
+    step2 find start/end angles
+    step3 find brightness threshold
+option to output calibratrion files:
+    1) center/radius
+    2) angle compass
+**/
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
 #include <opencv2/opencv.hpp>
 
 #undef DEBUG
@@ -10,123 +26,161 @@
 #define max(a,b) ((a<b)?b:a)
 #define dist_2_pts(x1,y1,x2,y2) (sqrt(pow((x2)-(x1),2)+pow((y2)-(y1),2)))
 #define rad2deg(a) ((a)*180.0/PI)
+#define ERROR -1
+
+bool verbose = false;
+bool dryRun = false;
+bool calibrate_step1 = false;
+bool calibrate_step2 = false;
+bool calibrate_step3 = false;
 
 using namespace cv;
 int main(int argc, char** argv )
 {
-    if ( argc != 2 )
-    {
-        printf("usage: DisplayImage.out <Image_Path>\n");
-        return -1;
-    }
-
-    Mat img;
-    img = imread( argv[1], 1 );
-    if ( !img.data )
-    {
-        printf("No image data \n");
-        return -1;
-    }
-#ifdef DEBUG
-    printf("Loaded ok\n");
-#endif
-
-    int r = 83;
-    Point2i c(410,240);
-#ifdef CALIBRATE
-    int height, width;
-    height = img.rows;
-    width = img.cols;
-#ifdef DEBUG
-    printf("cols %d, rows %d \n", width, height);
-#endif
-
-    Mat gray;
-    cvtColor(img, gray, COLOR_BGR2GRAY);
-    imwrite("gray.jpg", gray);
-
-    std::vector<Vec3f> circles;
-    HoughCircles(gray, circles, HOUGH_GRADIENT,
-                 1, 20, 100, 50); //, 40, 80);
-    //           1, 20, 100, 50, int(height*0.15), int(height*0.48));
-
-    int n = int(circles.size());
-    printf("Found %d circles.\n",n);
-
-    if ( n == 0 )
-       return 1;
-
-    for( size_t i = 0; i < n; i++ )
-    {
-         Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
-         int radius = cvRound(circles[i][2]);
-         // printf("[%d %d] %d\n", cvRound(circles[i][0]), cvRound(circles[i][1]), cvRound(circles[i][2]));
-         // printf("[%d %d] %d\n", center.x, center.y, radius);
-         // Point2f a(0.3f, 0.f), b(0.f, 0.4f);
-         // Point pt = (a + b)*10.f;
-         c += center;
-         r += radius;
-#ifdef DEBUG
-         std::cout << center << " " << radius << std::endl;
-#endif
-         // draw the circle center
-         circle( img, center, 3, Scalar(0,255,0), -1, 8, 0 );
-         // draw the circle outline
-         circle( img, center, radius, Scalar(0,0,255), 3, 8, 0 );
-    }
-    c /= n;
-    r = cvRound(r / n);
-#ifdef DEBUG
-    std::cout << "# " << c << " " << r << std::endl;
-#endif
-    // draw the circle center
-    circle( img, c, 3, Scalar(0,255,250), -1, 8, 0 );
-    // draw the circle outline
-    circle( img, c, r, Scalar(0,250,255), 3, 8, 0 );
-
-    /**
-    goes through the motion of a circle and sets x and y values based on the set separation spacing.  Also adds text to each
-    line.  These lines and text labels serve as the reference point for the user to enter
-    NOTE: by default this approach sets 0/360 to be the +x axis (if the image has a cartesian grid in the middle), the addition
-    (i+9) in the text offset rotates the labels by 90 degrees so 0/360 is at the bottom (-y in cartesian).  So this assumes the
-    gauge is aligned in the image, but it can be adjusted by changing the value of 9 to something else.
-    **/
-    float separation = 10.0f; //in degrees
-    int interval = int(360 / separation);
-    Point2i p1[interval]; // set empty arrays
-    Point2i p2[interval];
-    Point2i p_text[interval];
-    for ( int i=0; i<interval; i++) {
-        p1[i].x = c.x + 0.9 * r * cos(separation * i * 3.14 / 180); // #point for lines
-        p1[i].y = c.y + 0.9 * r * sin(separation * i * 3.14 / 180);
-    }
-
-    int text_offset_x = 10;
-    int text_offset_y = 5;
-    for ( int i=0; i<interval; i++) {
-        p2[i].x = c.x + r * cos(separation * i * 3.14 / 180);
-        p2[i].y = c.y + r * sin(separation * i * 3.14 / 180);
-        p_text[i].x = c.x - text_offset_x + 1.2 * r * cos((separation) * (i+9) * 3.14 / 180); // point for text labels, i+9 rotates the labels by 90 degrees
-        p_text[i].y = c.y + text_offset_y + 1.2* r * sin((separation) * (i+9) * 3.14 / 180); //  point for text labels, i+9 rotates the labels by 90 degrees
-    }
-
-    // add the lines and labels to the image
-    for ( int i=0; i<interval; i++) {
-        line(img, p1[i], p2[i], Scalar(0, 255, 0), 2);
-        putText(img, std::to_string(int(i*separation)), p_text[i], FONT_HERSHEY_SIMPLEX, 0.3f, Scalar(0,0,0),1,LINE_AA);
-#ifdef DEBUG
-        std::cout << ".. " << p1[i] << " " <<p2[i] << std::endl;
-#endif
-    }
-
-    imwrite("cerchi.jpg", img);
-#endif // CALIBRATE
-
+    int opt = 0;
+    char *fname = NULL;
+    int r;
+    Point2i c(0,0);
     int min_angle = 60;
     int max_angle = 320;
     int min_value = 0;
     int max_value = 4;
-    String units = "bar";
+
+    // worked: -r 83 -x 410 -y 240
+    
+    while ((opt = getopt(argc, argv, "vi:r::x::y::d")) != -1) {
+        switch(opt) {
+            case 'v':
+                verbose = true;
+                break;
+            case 'd':
+                dryRun = true;
+                break;
+            case 'r':
+                r = atoi(optarg);
+                break;
+            case 'x':
+                c.x = atoi(optarg);
+                break;
+            case 'y':
+                c.y = atoi(optarg);
+                break;
+            case 'i':
+                fname = optarg;
+                if (verbose) printf("\nInput file: %s", in_fname);
+                break;
+            case '?':
+                /* Case when user enters the command as
+                * $ ./cmd_exe -i
+                */
+                if (optopt == 'i') {
+                    fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+                    return ERROR;
+                } else if (isprint (optopt)) {
+                    fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+                    return ERROR;
+                } else {
+                    fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
+                }
+                break;
+            default:
+                abort();
+        }
+    }
+
+    if (dryRun)
+        return 0;
+
+    Mat img;
+    img = imread( fname, 1 );
+    if ( !img.data )
+    {
+        fprintf(stderr, "No image data.\n");
+        return -1;
+    }
+    if (verbose) printf("Loaded ok.\n");
+
+    if (calibrate_step1) {
+        int height, width;
+        height = img.rows;
+        width = img.cols;
+        if (verbose) printf("File cols %d, rows %d.\n", width, height);
+
+        Mat gray;
+        cvtColor(img, gray, COLOR_BGR2GRAY);
+        //imwrite("gray.jpg", gray);
+
+        std::vector<Vec3f> circles;
+        HoughCircles(gray, circles, HOUGH_GRADIENT, 1, 20, 100, 50);
+
+        int n = int(circles.size());
+        if (verbose) printf("Found %d circles.\n",n);
+
+        if ( n == 0 )
+            return ERROR;
+
+        for( size_t i = 0; i < n; i++ )
+        {
+            Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+            int radius = cvRound(circles[i][2]);
+            c += center;
+            r += radius;
+
+            if (verbose) std::cout << i << ": c/r " << center << "/" << radius << std::endl;
+
+            // draw the circle center
+            circle( img, center, 3, Scalar(128,128,128), -1, 8, 0 );
+            // draw the circle outline
+            circle( img, center, radius, Scalar(128,128,128), 3, 8, 0 );
+        }
+        c /= n;
+        r = cvRound(r / n);
+        if (verbose) std::cout << "avg : c/r " << c << "/" << r << std::endl;
+
+        // draw the circle center
+        circle( img, c, 3, Scalar(0,255,0), -1, 8, 0 );
+        // draw the circle outline
+        circle( img, c, r, Scalar(0,0,255), 3, 8, 0 );
+    }
+    // end of calibration step 1
+
+    if (calibrate_step2) {
+        /**
+        goes through the motion of a circle and sets x and y values based on the set separation spacing.  Also adds text to each
+        line.  These lines and text labels serve as the reference point for the user to enter
+        NOTE: by default this approach sets 0/360 to be the +x axis (if the image has a cartesian grid in the middle), the addition
+        (i+9) in the text offset rotates the labels by 90 degrees so 0/360 is at the bottom (-y in cartesian).  So this assumes the
+        gauge is aligned in the image, but it can be adjusted by changing the value of 9 to something else.
+        **/
+        float separation = 10.0f; //in degrees
+        int interval = int(360 / separation);
+        Point2i p1[interval]; // set empty arrays
+        Point2i p2[interval];
+        Point2i p_text[interval];
+        for ( int i=0; i<interval; i++) {
+            p1[i].x = c.x + 0.9 * r * cos(separation * i * 3.14 / 180); // #point for lines
+            p1[i].y = c.y + 0.9 * r * sin(separation * i * 3.14 / 180);
+        }
+
+        int text_offset_x = 10;
+        int text_offset_y = 5;
+        for ( int i=0; i<interval; i++) {
+            p2[i].x = c.x + r * cos(separation * i * 3.14 / 180);
+            p2[i].y = c.y + r * sin(separation * i * 3.14 / 180);
+            p_text[i].x = c.x - text_offset_x + 1.2 * r * cos((separation) * (i+9) * 3.14 / 180); // point for text labels, i+9 rotates the labels by 90 degrees
+            p_text[i].y = c.y + text_offset_y + 1.2* r * sin((separation) * (i+9) * 3.14 / 180); //  point for text labels, i+9 rotates the labels by 90 degrees
+        }
+
+        // add the lines and labels to the image
+        for ( int i=0; i<interval; i++) {
+            line(img, p1[i], p2[i], Scalar(0, 255, 0), 2);
+            putText(img, std::to_string(int(i*separation)), p_text[i], FONT_HERSHEY_SIMPLEX, 0.3f, Scalar(0,0,0),1,LINE_AA);
+            if (verbose) std::cout << ".. " << p1[i] << " " <<p2[i] << std::endl;
+        }
+
+        imwrite("cerchi.jpg", img);
+    }
+    // end of calibration step 2
 
     Mat gray2;
     cvtColor(img, gray2, COLOR_BGR2GRAY);
