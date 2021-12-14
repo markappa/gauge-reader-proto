576d6b97e8be2724685331ea7ad7e412cd15de84
root <root@local-gauge-reader.local.hass.io>
2021-12-14 20:20:02 +0100

    prepare env with cxxopts build (untested)

--- dev/prepareDev.sh
+++ dev/prepareDev.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-#apk add gcc 
-#apk add cmake
-#apk add make 
 #apk add opencv 
 apk add opencv-dev 
 apk add musl-dev 
 apk add g++
 
 alias ll="ls -la"
+
+# build cxxopts
+apk add git
+#apk add cmake
+#export CMAKE_CXX_COMPILER=g++
+apk add make 
+mkdir cxxopts
+cd cxxopts
+git pull https://github.com/jarro2783/cxxopts master
+make .
+make install
+cd ..
 
