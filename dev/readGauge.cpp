694b9ef2378912bc0b6b0769a6c98bbb2ccc88d9
root <root@local-gauge-reader.local.hass.io>
2021-12-14 20:22:36 +0100

    fixed and tested with command line options

--- dev/readGauge.cpp
+++ dev/readGauge.cpp
@@ -1,20 +1,17 @@
 /**
 TODO list:
-Accept calibration arguments from commandline
-Option to do Canny on input file
-commandline options to perform calibration:
+Done - Accept calibration arguments from commandline
+Done - Option to do Canny on input file
+Done - commandline options to perform calibration:
     step1 find center and radius
     step2 find start/end angles
     step3 find brightness threshold
-option to output calibratrion files:
+Done (implicit in calibration) - option to output calibratrion files:
     1) center/radius
     2) angle compass
 **/
 
 #include <stdio.h>
-//#include <stdlib.h>
-//#include <unistd.h>
-//#include <ctype.h>
 #include <opencv2/opencv.hpp>
 #include <cxxopts.hpp>
 
@@ -27,7 +24,7 @@ option to output calibratrion files:
 #define max(a,b) ((a<b)?b:a)
 #define dist_2_pts(x1,y1,x2,y2) (sqrt(pow((x2)-(x1),2)+pow((y2)-(y1),2)))
 #define rad2deg(a) ((a)*180.0/PI)
-#define ERROR -1
+#define ERROR (-1)
 
 bool verbose = false;
 bool dryRun = false;
@@ -35,6 +32,7 @@ bool calibrate_step1 = false;
 bool calibrate_step2 = false;
 bool calibrate_step3 = false;
 bool canny = false;
+bool save_threshold_image = false;
 
 using namespace cv;
 int main(int argc, char** argv )
@@ -42,9 +40,9 @@ int main(int argc, char** argv )
     //char *fname = NULL;
     std::string fname;
     int r = 83;
-    Point2i c(410,240);
-    int min_angle = 60;
-    int max_angle = 320;
+    Point2i c(405,242);
+    int min_angle = 58;
+    int max_angle = 322;
     int min_value = 0;
     int max_value = 4;
     int thresh = 95;
@@ -63,13 +61,17 @@ int main(int argc, char** argv )
         ("m,max-value", "Max value for b/w img.", cxxopts::value<int>()->default_value("255"))
         ("s,calibrate-step", "Calibration stemp number (1,2 or 3).", cxxopts::value<int>())
         ("n,canny", "Canny img before line detection.")
+        ("save_threshold_image", "Save b/w image.")
         ("v,verbose", "Verbose output", cxxopts::value<bool>()->default_value("false"))
         ("d,dry-run", "Just parse options and exit")
         ("h,help", "Print usage")
+        ("filename", "Input image file name", cxxopts::value<std::vector<std::string>>())
     ;
 
+    options.positional_help("filename");
+
+    options.parse_positional({"filename"});
     auto result = options.parse(argc, argv);
-    options.parse_positional({"filename"})
 
     if (result.count("help"))
     {
@@ -77,23 +79,62 @@ int main(int argc, char** argv )
       exit(0);
     }
     verbose = result["verbose"].as<bool>();
+    dryRun = result["dry-run"].as<bool>();
     canny = result["canny"].as<bool>();
-    if (result.count("filename"))
-        fname = result["filename"].as<std::string>();
-    r = result["radius"].as<int>();
-    c = result["center"].as<std::vector<int>>();
-    std::vector<int>> tmp;
-    tmp = result["angle-bounds"].as<std::vector<int>>>();
-    min_angle = tmp[0];   
-    max_angle = tmp[1];   
-    tmp = result["value-bounds"].as<std::vector<int>>>();
-    min_value = tmp[0];   
-    max_value = tmp[1];   
-    thresh = result["threshold"].as<int>();
-    maxValue = result["max-value"].as<int>();
-    r = result["radius"].as<int>();
+    save_threshold_image = result["save_threshold_image"].as<bool>();
+    if (result.count("radius"))
+        r = result["radius"].as<int>();
+    if (result.count("center"))
+    {
+        c.x = result["center"].as<std::vector<int>>()[0];
+        c.y = result["center"].as<std::vector<int>>()[1];
+    }
+    std::vector<int> tmp;
+    if (result.count("angle-bounds"))
+    {
+        tmp = result["angle-bounds"].as<std::vector<int>>();
+        min_angle = tmp[0];   
+        max_angle = tmp[1];   
+    }
+    if (result.count("value-bouns"))
+    {
+        tmp = result["value-bounds"].as<std::vector<int>>();
+        min_value = tmp[0];   
+        max_value = tmp[1];   
+    }
+    if (result.count("threshold"))
+        thresh = result["threshold"].as<int>();
+    if (result.count("max-value"))
+        maxValue = result["max-value"].as<int>();
 
-    if (dryRun)
+    if (result.count("calibrate-step"))
+    {
+        int i = result["calibrate-step"].as<int>();
+        if (verbose) std::cout << "Starting calibration step " << i << std::endl;
+        switch (i)
+        {
+            case 1:
+                calibrate_step1 = true;
+                break;
+            case 2:
+                calibrate_step2 = true;
+                break;
+            case 3:
+                calibrate_step3 = true;
+                break;
+            default:
+                std::cout << "Unexpexted step value." << std::endl;
+                exit ERROR;
+        }
+    }
+
+    if (result.count("filename"))
+    {
+        auto& s = result["filename"].as<std::vector<std::string>>()[0];
+        fname = s;
+    }
+
+    if (dryRun || verbose)
     {
         std::cout << "verbose " << verbose << std::endl;
         std::cout << "canny " << canny << std::endl;
@@ -104,7 +145,13 @@ int main(int argc, char** argv )
         std::cout << "value-bounds " << min_value << "," << max_value << std::endl;
         std::cout << "threshold " << thresh << std::endl;
         std::cout << "max-value " << maxValue << std::endl;
-        return 0;
+        std::cout << "calibrate-step " << calibrate_step1 << ":" <<calibrate_step2 << ":" <<calibrate_step3 << std::endl;
+
+        if (dryRun)
+        {
+            std::cout << std::endl << "Skipping operations ( dry run)." << std::endl;
+            return 0;
+        }
     }
 
     Mat img;
@@ -145,9 +192,9 @@ int main(int argc, char** argv )
             if (verbose) std::cout << i << ": c/r " << center << "/" << radius << std::endl;
 
             // draw the circle center
-            circle( img, center, 3, Scalar(128,128,128), -1, 8, 0 );
+            circle( img, center, 3, Scalar(0,0,0), -1, 8, 0 );
             // draw the circle outline
-            circle( img, center, radius, Scalar(128,128,128), 3, 8, 0 );
+            circle( img, center, radius, Scalar(0,0,0), 3, 8, 0 );
         }
         c /= n;
         r = cvRound(r / n);
@@ -157,6 +204,8 @@ int main(int argc, char** argv )
         circle( img, c, 3, Scalar(0,255,0), -1, 8, 0 );
         // draw the circle outline
         circle( img, c, r, Scalar(0,0,255), 3, 8, 0 );
+
+        imwrite("calibration_step1.jpg", img);
     }
     // end of calibration step 1
 
@@ -173,6 +222,12 @@ int main(int argc, char** argv )
         Point2i p1[interval]; // set empty arrays
         Point2i p2[interval];
         Point2i p_text[interval];
+
+        // draw the circle center
+        circle( img, c, 3, Scalar(0,255,0), -1, 8, 0 );
+        // draw the circle outline
+        circle( img, c, r, Scalar(0,0,255), 3, 8, 0 );
+
         for ( int i=0; i<interval; i++) {
             p1[i].x = c.x + 0.9 * r * cos(separation * i * 3.14 / 180); // #point for lines
             p1[i].y = c.y + 0.9 * r * sin(separation * i * 3.14 / 180);
@@ -194,7 +249,7 @@ int main(int argc, char** argv )
             if (verbose) std::cout << ".. " << p1[i] << " " <<p2[i] << std::endl;
         }
 
-        imwrite("cerchi.jpg", img);
+        imwrite("calibration_step2.jpg", img);
     }
     // end of calibration step 2
 
@@ -204,7 +259,7 @@ int main(int argc, char** argv )
     // Set threshold and maxValue
     Mat dst2;
 
-    if (calibrate_3) {
+    if (calibrate_step3) {
         for(thresh= 80; thresh<120; thresh += 5) {
             // apply thresholding which helps for finding lines
             threshold(gray2, dst2, thresh, maxValue, THRESH_BINARY_INV);
@@ -216,13 +271,13 @@ int main(int argc, char** argv )
 
             //dst2 = cv2.GaussianBlur(dst2, (5, 5), 0)
             // show image after thresholding
-            imwrite("soglia"+ std::to_string(thresh) +".jpg", dst2);
+            imwrite("calibration_step3_"+ std::to_string(thresh) +".jpg", dst2);
          }
          // end of calibration step 3
     } else {
          threshold(gray2, dst2, thresh, maxValue, THRESH_BINARY_INV);
          if(save_threshold_image)
-             imwrite("soglia.jpg", dst2);
+             imwrite("threshold.jpg", dst2);
     }
 
     // find lines
