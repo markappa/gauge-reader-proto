2f9213f4643d3abb75be81c07a9dc5c867e98563
root <root@local-gauge-reader.local.hass.io>
2021-12-23 17:48:15 +0100

    chech if build is needed

--- dev/build.sh
+++ dev/build.sh
@@ -1,4 +1,11 @@
+#/bin/bash
+set -xT
 rm *.jpg 2>/dev/null
-g++ readGauge.cpp -o readGauge -I/usr/include/opencv4/ -Wl,/usr/lib/libopencv_core.so,/usr/lib/libopencv_imgcodecs.so,/usr/lib/libopencv_highgui.so,/usr/lib/libopencv_imgproc.so
+if [ readGauge.cpp -nt readGauge ] ; then
+  g++ readGauge.cpp -o readGauge -I/usr/include/opencv4/ -Wl,/usr/lib/libopencv_core.so,/usr/lib/libopencv_imgcodecs.so,/usr/lib/libopencv_highgui.so,/usr/lib/libopencv_imgproc.so
+fi
 
-cp readGauge .. 2>/dev/null
+if [ readGauge -nt ../readGauge ] ; then
+  cp readGauge .. 
+fi
+
